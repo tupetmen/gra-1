bad497a954e5b62b2fd284f565da1ffe108f9d1d
Łukasz Płotka <tupetmen@gmail.com>
2019-03-03 20:11:57 +0100

    add codepen files again

--- js/index.js
+++ js/index.js
@@ -8,41 +8,62 @@ var outputHuman = document.getElementById('human');
 var outputComputer = document.getElementById('computer');
 var gameContainer = document.getElementById('game');
 var gameOver = document.getElementById('game-over');
+ var gameOverContent = document.getElementsByClassName('content game-over');
 var score = document.getElementById('score');
-var playerWin = 0;
-var aiWin = 0;
-
-function finishGame(){
-  console.log("finish game dzialasss"); 
-  gameContainer.classList.add("hide");
-  if(playerWin>aiWin){
-    gameOver.innerHTML = ' Wygrałeś ze mną :(';
-  }
-  else{
-    gameOver.innerHTML = 'Ha! Wiedziałem, że wygram :D';
-  }
-  gameOver.classList.remove("hide");
-  playerWin = 0;
-  result.querySelector('#wins').innerHTML = playerWin;
-  aiWin = 0;
-  result.querySelector('#losses').innerHTML = aiWin;
-  outputComputer.innerHTML = '';
-  outputHuman.innerHTML = '';
-  score.innerHTML =''
-}
+var maxWin;
+var scoreHelp = [ " 0 ", " 1 " ];
+var scoreHelp2;
+var computerWinResult = " 0 : 1 ";
+var thereIsNoWinnerResult = " 0 : 0 ";
+var scoreList;
+var scorePrint = [];
+var buttonNewGame = document.getElementById('new-game');
+var params = {
+  playerWin: 0,
+  aiWin: 0,
+  round: 0,
+  progress: {"runda":"0",
+             "ruch gracza":"0",
+             "ruch komputera":"0", 
+             "wynik rundy":"0", 
+             "wynik gry":"0"}
+  
+  
+  
+};
+console.log(gameOverContent);
+// console.log(scoreHelp);
+//nasłuch na kliknięcie
 buttonNewGame.addEventListener('click', function(){
 //  gameContainer.classList.add("hide"); 
   maxWin = window.prompt('Do ilu wygranych chcesz grać?');
-  gameOver.innerHTML = '';
+  resetScore();
   if(maxWin){
     maxWin = Number(maxWin);
     if(maxWin > 0){
- //początek warunków jeżeli podał liczbe większą od 0      
+//początek warunków jeżeli podał liczbe większą od 0      
       maxWinnerGames.querySelector('#max-games').innerHTML = maxWin;
-      button1.addEventListener('click', playerMove);
-      button2.addEventListener('click', playerMove);
-      button3.addEventListener('click', playerMove);
-//koniec warunków jeżeli podał liczbe większą od 0  
+      
+      var buttons = document.getElementsByClassName('player-move');
+      
+      for(var i = 0; i < buttons.length; i++){
+     //   console.log("test");
+        //buttons.forEach(function(button, index){
+        (function(){
+          
+          var button = buttons[i];
+          var buttonName =  button.getAttribute('data-move');
+          button.addEventListener('click',function(){
+          playerMove(buttonName);
+          })
+         }());
+      };
+       
+        //console.log(buttonsName, i);
+      //  buttonsName = buttons[i].getAttribute('data-move');
+      
+      
+      
       error.querySelector('#test-message').innerHTML = '';
       gameContainer.classList.remove("hide");  
     }
@@ -56,47 +77,135 @@ buttonNewGame.addEventListener('click', function(){
   else{
     error.querySelector('#test-message').innerHTML = 'wpisz cos';
   }  
-}                              
-);
-var playerMove = function(event){
+});
+
+//zmienna dla funkcji gry
+var playerMove = function(buttonsName){
     // co wybrał user  
    // output.innerHTML = '';
-    if(this == button1){outputHuman.innerHTML = '<br>User wybrał: <b>Papier</b>';}
-    else if(this == button2){outputHuman.innerHTML = '<br>User wybrał: <b>Kamień</b>';}
-    else if(this == button3){outputHuman.innerHTML = '<br>User wybrał: <b>Nożyce</b>';}
+// console.log(buttonsName);
+    if(buttonsName == "paper"){outputHuman.innerHTML = '<br>User wybrał: <b>Papier</b>';}
+    else if(buttonsName == "stone"){outputHuman.innerHTML = '<br>User wybrał: <b>Kamień</b>';}
+    else if(buttonsName == "scissors"){outputHuman.innerHTML = '<br>User wybrał: <b>Nożyce</b>';}
     //co wybrał komputer
     var rand =  Math.floor(Math.random()*3+1);
-    if(rand == 1){outputComputer.innerHTML = '<br>Komputer wybrał: <b>Papier</b>';}
-    else if(rand == 2){outputComputer.innerHTML = '<br>Komputer wybrał: <b>Kamień</b>';}
-    else if(rand == 3){outputComputer.innerHTML = '<br>Komputer wybrał: <b>Nożyce</b>';}
+    if(rand == 1){outputComputer.innerHTML = '<br>Komputer wybrał: <b>Papier</b>'; var randName = "paper";}
+    else if(rand == 2){outputComputer.innerHTML = '<br>Komputer wybrał: <b>Kamień</b>'; var randName = "stone";}
+    else if(rand == 3){outputComputer.innerHTML = '<br>Komputer wybrał: <b>Nożyce</b>'; var randName = "scissors";}
     //jaki jest wynik 
-    if(this == button1 && rand == 1){score.innerHTML ='<br>REMIS';}
-    else if(this == button2 && rand == 2){score.innerHTML ='<br>REMIS';}
-    else if(this == button3 && rand == 3){score.innerHTML ='<br>REMIS';}
-    else if(this == button1 && rand == 2){score.innerHTML ='<br>WYGRAŁEŚ';
-                                         playerWin++;
-                                          result.querySelector('#wins').innerHTML = playerWin;}
-    else if(this == button1 && rand == 3){score.innerHTML = '<br>PRZEGRAŁEŚ';
-                                          aiWin++;
-                                          result.querySelector('#losses').innerHTML = aiWin;}
-    else if(this == button2 && rand == 1){score.innerHTML = '<br>PRZEGRAŁEŚ';
-                                          aiWin++;
-                                         result.querySelector('#losses').innerHTML = aiWin;}
-    else if(this == button2 && rand == 3){score.innerHTML = '<br>WYGRAŁEŚ';
-                                          playerWin++;
-                                         result.querySelector('#wins').innerHTML = playerWin;}
-    else if(this == button3 && rand == 1){score.innerHTML = '<br>WYGRAŁEŚ';
-                                         playerWin++;
-                                         result.querySelector('#wins').innerHTML = playerWin;}
-    else if(this == button3 && rand == 2){score.innerHTML = '<br>PRZEGRAŁEŚ';
-                                          aiWin++;
-                                         result.querySelector('#losses').innerHTML = aiWin;}
-  if(maxWin==playerWin || maxWin==aiWin){
+    if(buttonsName == "paper" && rand == 1){score.innerHTML ='<br>REMIS';
+                                          params.round++;
+                                         scoreHelp2 = scoreHelp[0] + " : " + scoreHelp[0];         
+                                                }
+    else if(buttonsName == "stone" && rand == 2){score.innerHTML ='<br>REMIS';
+                                          params.round++;
+                                          scoreHelp2 = scoreHelp[0] + " : " + scoreHelp[0];         
+                                                }
+    else if(buttonsName == "scissors" && rand == 3){score.innerHTML ='<br>REMIS';
+                                          params.round++;
+                                          scoreHelp2 = scoreHelp[0] + " : " + scoreHelp[0];            
+                                                 }
+    else if(buttonsName == "paper" && rand == 2){score.innerHTML ='<br>WYGRAŁEŚ';
+                                         params.playerWin++;
+                                         params.round++;  
+                                         scoreHelp2 = scoreHelp[1] + " : " + scoreHelp[0];          
+                                          result.querySelector('#wins').innerHTML = params.playerWin;}
+    else if(buttonsName == "paper" && rand == 3){score.innerHTML = '<br>PRZEGRAŁEŚ';
+                                          params.aiWin++;
+                                          params.round++;
+                                          scoreHelp2 = scoreHelp[0] + " : " + scoreHelp[1];         
+                                          result.querySelector('#losses').innerHTML = params.aiWin;}
+    else if(buttonsName == "stone" && rand == 1){score.innerHTML = '<br>PRZEGRAŁEŚ';
+                                          params.aiWin++;
+                                          params.round++;
+                                          scoreHelp2 = scoreHelp[0] + " : " + scoreHelp[1];         
+                                         result.querySelector('#losses').innerHTML = params.aiWin;}
+    else if(buttonsName == "stone" && rand == 3){score.innerHTML = '<br>WYGRAŁEŚ';
+                                          params.playerWin++;
+                                          params.round++;
+                                          scoreHelp2 = scoreHelp[1] + " : "  + scoreHelp[0];         
+                                         result.querySelector('#wins').innerHTML = params.playerWin;}
+    else if(buttonsName == "scissors" && rand == 1){score.innerHTML = '<br>WYGRAŁEŚ';
+                                         params.playerWin++;
+                                         params.round++;
+                                          scoreHelp2 = scoreHelp[1] + " : " + scoreHelp[0];            
+                                         result.querySelector('#wins').innerHTML = params.playerWin;
+                                                   }
+    else if(buttonsName == "scissors" && rand == 2){score.innerHTML = '<br>PRZEGRAŁEŚ';
+                                          params.aiWin++;
+                                          params.round++;
+                                         scoreHelp2 = scoreHelp[0] + " : " + scoreHelp[1];            
+                                         result.querySelector('#losses').innerHTML = params.aiWin;}
+ // console.log(params.aiWin);
+ // console.log(params.playerWin);
+
+    
+  scoreList = "<tr><td>" +  params.round + 
+    "</td><td>" + buttonsName + 
+    "</td><td>" + randName +
+    "</td><td>" + params.playerWin +
+    "</td><td>" + params.aiWin + 
+    "</td><td>" + scoreHelp2 + "</td></tr>";
+  
+  scorePrint.push(scoreList);
+  
+
+  
+  if(maxWin==params.playerWin || maxWin==params.aiWin){
     finishGame()
-  }  
+  }
 };
-//przypisanie zmiennych do buttonow
-var button1 = document.getElementById('button1');
-var button2 = document.getElementById('button2');
-var button3 = document.getElementById('button3');
-//przypisanie akcji do buttonow
+
+  var scorePrint2 = function(){
+    var watchingScores;
+    for(var i = 0; i < scorePrint.length; i++){
+		  watchingScores += scorePrint[i];
+    };
+    return "<table><thead><tr><th scope=\"col\">Runda</th><th scope=\"col\">Gracz</th><th scope=\"col\">Komputer</th><th scope=\"col\">Wynik gracza</th><th scope=\"col\">Wynik komputera</th><th scope=\"col\">Wynik tej rundy</th></tr></thead><tbody>"+watchingScores+"</tbody></table>";
+    console.log(scorePrint[0]);
+  };
+
+function finishGame(){
+// console.log(scorePrint);  
+scorePrint2();
+  console.log(scorePrint2());
+  if(params.playerWin>params.aiWin){
+    
+    gameOver.innerHTML = ' Wygrałeś ze mną :(';
+    gameOverContent[0].innerHTML = scorePrint2();
+    document.querySelector('#modal-overlay').classList.add('show');
+    modalWindows();
+  }
+  else{
+    gameOver.innerHTML = 'Ha! Wiedziałem, że wygram :D';
+    gameOverContent[0].innerHTML = scorePrint2();
+    document.querySelector('#modal-overlay').classList.add('show');
+    
+    modalWindows();
+  }
+}
+
+var modalWindows = (function(){ 
+	var hideModal = function(event){
+		event.preventDefault();
+		document.querySelector('#modal-overlay').classList.remove('show');
+    gameContainer.classList.add("hide");
+	};
+	var closeButtons = document.querySelectorAll('.modal .close');
+	for(var i = 0; i < closeButtons.length; i++){
+		closeButtons[i].addEventListener('click', hideModal);
+  }
+
+})(); 
+
+var resetScore = function(){  
+  gameOver.innerHTML = '';
+  params.playerWin = 0;
+  result.querySelector('#wins').innerHTML = params.playerWin;
+  params.aiWin = 0;
+  params.round = 0;
+  result.querySelector('#losses').innerHTML = params.aiWin;
+  outputComputer.innerHTML = '';
+  outputHuman.innerHTML = '';
+  score.innerHTML =''
+  }
